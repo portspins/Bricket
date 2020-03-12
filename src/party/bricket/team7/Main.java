f881916265d2f3f85cf1d44aec56bb39a982955a
portspins <mrh0036@uah.edu>
2020-03-12 14:18:38 -0500

    Added basic skeleton for testing the three search-related classes

---
+++ src/party/bricket/team7/Main.java
@@ -0,0 +1,12 @@
+package party.bricket.team7;
+
+public class Main {
+    public static void main(String[] args) {
+        // Get a search query from the user
+
+        // Create a new Search object with a BricksetScraper object and the query
+        
+        // Iterate through Search's SearchResults and print out the info from each
+
+    }
+}
