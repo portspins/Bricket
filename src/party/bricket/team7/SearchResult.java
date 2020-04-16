c1bf1f20b89932f9a5c14a33eeba99a895ae0bba
portspins <mrh0036@uah.edu>
2020-04-15 20:49:32 -0500

    Updated SearchResult to trimmed version since some things got moved to ResearchResult

--- src/party/bricket/team7/SearchResult.java
+++ src/party/bricket/team7/SearchResult.java
@@ -1,217 +1,70 @@
 package party.bricket.team7;
 
 import java.util.*;
+
 /** party.bricket.team7.SearchResult: A class that will act as a data type for a single item
  * from a brickset search's result list.
- * Stored inside are:
- * ID#, Brickset page link, Thumbnail link, Name, MSRP, Theme, Release Date,
- * Part Count, Retirement Date (if applicable), and a list of Minifigures in the set.
+ * Stores ID#, Brickset page link, Thumbnail link, Name, Release Year
  * @author Daniel Morris
  */
 public class SearchResult {
-    private String idNumber = "";                                           // ID number in "####-####" format
-    private String brickset = "";                                       // Brickset Link
-    private String thumbnail = "";                                      // Thumbnail Link
-    private String name = "";                                           // Set Name
-    private double msrp = 0.0;                                          // Manufacturer Suggested Retail Price
-    private String theme = "";                                          // Theme of the set (Police, City, etc.)
-    private String releaseDate = "";                                    // Release Date of set
-    private int partCount = 0;                                          // Part Count of the set
-    private boolean retired = false;                                       // Retired Flag
-    private String retireDate = "";                                     // Date of retirement, if applicable
-    private ArrayList<String> minifigNames = new ArrayList<>();   // List of Minifigs in the set
+    private String id;                                                  // ID number in "####-####" format
+    private String name;                                                // Item name
+    private int releaseYear;                                            // Item release year
+    private String itemLink;                                            // Item's Brickset Link
+    private String thumbnailLink;                                       // Thumbnail Link
+
+
     /** Full Constructor. Takes all inputs to fill out the attributes.
      */
-    public SearchResult(String idNumberIn, String bricksetIn, String thumbnailIn, String nameIn, double msrpIn, String themeIn, String releaseDateIn, int partCountIn, boolean retiredFlag, String retireDateIn, ArrayList<String> minifigNamesIn) {
-        idNumber = idNumberIn;
-        brickset = bricksetIn;
-        thumbnail = thumbnailIn;
-        name = nameIn;
-        msrp = msrpIn;
-        theme = themeIn;
-        releaseDate = releaseDateIn;
-        partCount = partCountIn;
-        retired = retiredFlag;
-        retireDate = retireDateIn;
-        minifigNames = minifigNamesIn;
+    public SearchResult(String id, String name, int releaseYear, String itemLink, String thumbnailLink) {
+        this.id = id;
+        this.name = name;
+        this.releaseYear = releaseYear;
+        this.itemLink = itemLink;
+        this.thumbnailLink = thumbnailLink;
     }
 
     /**
-     * constructor with no inputs
+     * Default constructor
      */
     public SearchResult() {
-        name = ""; // just needed something here I guess
+
     }
 
-    /** GetIDNumber.
+    /** GetId
      *Retrieves a copy of the id number.
      */
-    public String getIdNumber() {
-        String decoy = idNumber;
-        return decoy;
+    public String getId() {
+        return id;
     }
 
-    /**SetIDNumber.
-     *Sets the id number to the input parameter.
+    /** GetItemLink.
+     *Retrieves the brickset link.
      */
-    public boolean setIdNumber(String id) {
-        idNumber = id;
-        return true;
-    }
-
-    /**GetBSLink.
-     *Retrieves a copy of the brickset link.
-     */
-    public String getBSLink() {
-        String decoy = brickset;
-        return decoy;
-    }
-
-    /**SetBSLink.
-     *Sets the brickset link to the input parameter.
-     */
-    public boolean setBSLink(String link) {
-        brickset = link;
-        return true;
+    public String getItemLink() {
+        return itemLink;
     }
 
     /**getThumbnail.
-     *Retrieves a copy of the thumbnail link.
+     *Retrieves the thumbnail link.
      */
-    public String getThumbnail() {
-        String decoy = thumbnail;
-        return decoy;
-    }
-
-    /**setThumbnail.
-     *Sets the thumbnail link to the input parameter.
-     */
-    public boolean setThumbnail(String TN) {
-        thumbnail = TN;
-        return true;
+    public String getThumbnailLink() {
+        return thumbnailLink;
     }
 
     /**getName.
-     *Retrieves a copy of the name.
+     *Retrieves the name.
      */
     public String getName() {
-        String decoy = name;
-        return decoy;
+        return name;
     }
 
-    /**setName.
-     *Sets the name to the input parameter.
+    /**getReleaseYear
+     * Retrieves the release year.
      */
-    public boolean setName(String nameIn) {
-        name = nameIn;
-        return true;
-    }
-
-    /**getMSRP
-     *Retrieves a copy of the msrp.
-     */
-    public double getMSRP() {
-        double decoy = msrp;
-        return decoy;
-    }
-
-    /**setMSRP
-     *Sets the msrp to the input parameter.
-     */
-    public boolean setMSRP(double price) {
-        msrp = price;
-        return true;
-    }
-
-    /**getTheme
-     *Retrieves a copy of the theme.
-     */
-    public String getTheme() {
-        String decoy = theme;
-        return decoy;
-    }
-
-    /**setTheme
-     *Sets the name to the input parameter.
-     */
-    public boolean setTheme(String thm) {
-        theme = thm;
-        return true;
-    }
-
-    /**getReleaseDate
-     *Retrieves a copy of the Release Date.
-     */
-    public String getReleaseDate() {
-        String decoy = releaseDate;
-        return decoy;
-    }
-
-    /**setReleaseDate
-     *Sets the release date to the input parameter.
-     */
-    public boolean setReleaseDate(String rd) {
-        releaseDate = rd;
-        return true;
-    }
-
-    /**getPC
-     *Retrieves a copy of the part count.
-     */
-    public int getPC() {
-        int decoy = partCount;
-        return decoy;
-    }
-
-    /**setPC
-     *Sets the part count to the input parameter.
-     */
-    public boolean setPC(int pc) {
-        partCount = pc;
-        return true;
-    }
-
-    /**getRetireFlag
-     *Retrieves a copy of the retire flag.
-     */
-    public boolean getRetireFlag() {
-        boolean decoy = retired;
-        return decoy;
-    }
-
-    //There is only a get method for the Retired flag
-
-    /**getRetireDate
-     *Retrieves a copy of the retirement date.
-     */
-    public int getRetireDate() {
-        int decoy = partCount;
-        return decoy;
-    }
-
-    /**setRetireDate
-     *Sets the part count to the retirement date.
-     */
-    public boolean setRetirementDate(String rt) {
-        retireDate = rt;
-        retired = rt.compareTo("") != 0;
-        return true;
-    }
-
-    /**getList
-     *Retrieves a copy of the minifig list.
-     */
-    public ArrayList<String> getList() {
-        ArrayList<String> decoy = minifigNames;
-        return decoy;
-    }
-
-    /**setList
-     *Sets the minifig list to the input parameter.
-     */
-    public boolean setList(ArrayList<String> minis) {
-        minifigNames = minis;
-        return true;
+    public int getReleaseYear() {
+        return releaseYear;
     }
 
     /**toString()
@@ -219,7 +72,6 @@ public class SearchResult {
      * data that will be used in the list created by the GUI.
      */
     public String toString() {
-        String excerpt = idNumber + " " + thumbnail + " " + name + " " + theme;
-        return excerpt;
+        return id + " " + name + " " + releaseYear;
     }
 }
