8b7d3f286bfdaa9c6e8d61fffb5d43e7e2034ef4
dwm04 <dwm0010@uah.edu>
2020-04-17 17:24:45 -0400

    Interface ResearchIO Version 2 JSON File Format By Daniel Morris

--- src/party/bricket/team7/ResearchIO.java
+++ src/party/bricket/team7/ResearchIO.java
@@ -1,42 +1,78 @@
 package party.bricket.team7;
 
-import java.io.File;
+import java.io.*;
+import org.json.*;
 
-/** party.bricket.team7.ResearchIO: An interface for Speculator that will enable the saving
- * and loading of a ResearchResult
- * Save a ResearchResult as a file, store its data, and implement a "load" option to open saved ResearchResult files
+
+/** party.bricket.team7.ResearchIO: A class extended by Speculator that will enable
+ * the saving and loading of a ResearchResult.
+ * Save a ResearchResult as a file, store its data, and implement a "load" option
+ * to open saved ResearchResult files.
  * Refresh method to restore the original data for a speculator
  * @author Daniel Morris
  */
-public interface ResearchIO {
+public class ResearchIO {
+
     /** research File.
      * stores the data of the most recently opened ResearchResult file.
      * change to store the selected ResearchResult file in its tab later.
      */
-    File research = new File("");
-
-    /** GetItemLink.
-     *Retrieves the brickset link.
-     */
-    //ResearchResult stored = new ResearchResult();
+    private File research = new File("DefaultFile.json");
+    //Possibly needs replaced with JSON implementation
 
     /** loadResearch.
-     * Accepts a file name String and returns the stored
-     * ResearchResult. Stores the loaded file in research.
+     * Accepts a JSON file name string and adds the stored
+     * ResearchResult into Speculator. Stores the loaded
+     * file in research.
+     * Check for fail to open exception.
      * Need a consistent naming scheme for files.
      */
-    ResearchResult loadResearch(String id);
+    public ResearchResult loadResearch(String id)
+    {
+        //store string
+        //open json file with string name
+        //create ResearchResult with json file data
+        ResearchResult loadHusk=new ResearchResult(); //**FIX**
+        //add ResearchResult to Speculator
+        return loadHusk;
+    }
 
     /** saveResearch.
-     * Saves the researchResult in the Speculator into a file.
-     * Need a consistent naming scheme for files
+     * Saves the researchResult provided into a JSON file.
+     * Need a consistent naming scheme for files.
      */
-    boolean saveResearch(ResearchResult s);
+    public boolean saveResearch(ResearchResult s)
+    {
+        //create JSON Object
+        JSONObject toSave = new JSONObject();
+        //store ResearchResult data in JSON Object
+        toSave.put("id", s.getID());
+        toSave.put("name", s.getName());
+        toSave.put("theme", s.getTheme());
+        toSave.put("bricksetLink", s.getBricksetLink());
+        toSave.put("imageLink", s.getImageLink());
+        toSave.put("retiredFlag", s.isRetired());
 
-    /** refreshResearch.
-     * (???) Resets the researchResult parameter's components to
-     * the stored ResearchResult. Returns reset copy of stored
-     * ResearchResult
-     */
-    ResearchResult refreshResearch(ResearchResult s);
+        //Editable data
+        toSave.put("retailPrice", s.getRetailPrice());
+        toSave.put("value", s.getValue());
+        toSave.put("pricePerPart", s.getPricePerPart());
+        toSave.put("rating", s.getRating());
+        toSave.put("minifigList", s.getMinifigList()); //Needs to get entire ArrayList
+        toSave.put("partCount", s.getPartCount());
+        toSave.put("releaseDate", s.getReleaseDate());
+        toSave.put("retireDate", s.getRetireDate());
+
+        //create JSON file with constant format
+        try {
+            FileWriter save = new FileWriter(research);
+            //Write JSON Object into file
+            save.write(toSave.toString());
+            save.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //return boolean, true if saving was successful, false if not.
+        return false;
+    }
 }
